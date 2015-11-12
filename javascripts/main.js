a71597074293c3d78987dc328a41cc95b814c454
Sequina Caro <sccaro89@gmail.com>
2015-11-11 19:48:56 -0600

    was able to get song titles to display

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,24 +1,76 @@
-var app = angular.module("MusicApp", [])
+var app = angular.module("MusicApp", ["ngRoute"]);
 
-app.controller("MusicCtrl", function($scope, $rootScope) {
-    $scope.title = "Welcome to your Music App"
-    $rootScope.hello ="i am everywhere"
+app.config (['$routeProvider',
+  function($routeProvider) {
+    $routeProvider
+    .when('/songs/list', {
+      templateUrl: 'partials/song-list.html',
+      controller: 'SongListCtrl'
+    })
+    .when('/songs/new', {
+      templateUrl: 'partials/song-form.html',
+      controller: 'SongCtrl'
+        })
+    }]);
 
-    $scope.artists = [
-      {name:"Taylor Swift"},
-      {name:"The Weekend"},
-      {name:"Demi Lovato"},
-      {name:"Carrie Underwood"},
-      {name:"Rihanna"},
-      {name:"Eminem"},
-      {name:"Christina Aguilera"}
-    ];
+app.controller("SongListCtrl",
+  // Notice the new syntax. Since I'm including one of my own dependencies
+  // then I need to include each one in array of strings (just like RequireJS)
+  // and have a matching argument in the callback function.
+  [
+    "$scope", "$q", "$http",
+    function($scope, $q,$http) {
+      $scope.songList = [];
+      // $scope.song_list = simple_songs.getSongs();  // Returns all songs
+        return $q(function(resolve, reject) {
+        $http
+          .get('./javascripts/songs.json')
+          .success(
+            function(mySongs) {
+              $scope.songList = mySongs.songs;
+              resolve(mySongs.songs);
+            },function(error) {
+              reject(error);
+            }
+          );
+      });
+    }
+  ]
+);
+app.controller("SongCtrl",
+  [
+    "$scope",
+    function($scope) {
+      // $scope.song_list = simple_songs.getSongs();  // Returns all songs
 
-$scope.killMusicArtists=function(artist) {
-      var musicIndex = $scope.artists.indexOf(artist);
+    }
+  ]
+);
 
-    if(musicIndex>=0) {
-      $scope.artists.splice(musicIndex, 1)
-    };
-  };
-})
+
+
+
+
+
+// app.controller("SongListCtrl", function($scope, $rootScope,$q,$http) {
+//     $scope.title = "Welcome to your Music App";
+//     $rootScope.hello ="i am everywhere";
+
+//     // $scope.artists = [
+//     //   {name:"Taylor Swift"},
+//     //   {name:"The Weekend"},
+//     //   {name:"Demi Lovato"},
+//     //   {name:"Carrie Underwood"},
+//     //   {name:"Rihanna"},
+//     //   {name:"Eminem"},
+//     //   {name:"Christina Aguilera"}
+//     // ];
+
+// $scope.killMusicArtists=function(artist) {
+//       var musicIndex = $scope.artists.indexOf(artist);
+
+//     if(musicIndex>=0) {
+//       $scope.artists.splice(musicIndex, 1)
+//     };
+//   };
+// })
